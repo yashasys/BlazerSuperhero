c35061f6b5bd90462ffb64f79245ed52f60c145f
R <yashas.r@dxc.com>
2022-03-29 17:27:17 +0530

    Updateing SuperHero

---
+++ SuperHeroDB/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroDB.Server.Data;
+using SuperHeroDB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        static List<Comic> comics = new List<Comic> {
+            new Comic { Id = 1, Name = "Marvel"},
+            new Comic { Id = 2, Name = "DC"}
+        };
+
+        static List<SuperHero> heroes = new List<SuperHero> {
+            new SuperHero { Id = 1, FirstName = "Peter", LastName = "Parker", HeroName = "Spiderman", Comic = comics[0] },
+            new SuperHero { Id = 2, FirstName = "Bruce", LastName = "Wayne", HeroName = "Batman", Comic = comics[1] },
+        };
+
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("comics")]
+        public async Task<IActionResult> GetComics()
+        {
+            return Ok(await _context.Comics.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSuperHeroes()
+        {
+            return base.Ok(await GetDbHeroes());
+        }
+
+        private async Task<List<SuperHero>> GetDbHeroes()
+        {
+            return await _context.SuperHeroes.Include(sh => sh.Comic).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingleSuperHero(int id)
+        {
+            var hero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(h => h.Id == id);
+            if (hero == null)
+                return NotFound("Super Hero wasn't found. Too bad. :(");
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSuperHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSuperHero(SuperHero hero, int id)
+        {
+            var dbHero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(h => h.Id == id);
+            if (dbHero == null)
+                return NotFound("Super Hero wasn't found. Too bad. :(");
+
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.HeroName = hero.HeroName;
+            dbHero.ComicId = hero.ComicId;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSuperHero(int id)
+        {
+            var dbHero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(h => h.Id == id);
+            if (dbHero == null)
+                return NotFound("Super Hero wasn't found. Too bad. :(");
+
+            _context.SuperHeroes.Remove(dbHero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+    }
+}
---
+++ SuperHeroDB/Shared/SuperHero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Shared
+{
+    public class SuperHero
+    {
+        public int Id { get; set; } = 0;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HeroName { get; set; }
+        public Comic Comic { get; set; }
+        public int ComicId { get; set; }
+    }
+}
